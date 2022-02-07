cdbc9db2a480a9fded44693fffeb963e64e0c991
Alan Silva <alan.bezerra@xpi.com.br>
2022-02-07 02:47:44 -0300

    feature: Layout Truco Activity

--- PlacardaGeral/app/src/main/java/com/alansilva/placardageral/presentation/game/types/TrucoScoreActivity.kt
+++ PlacardaGeral/app/src/main/java/com/alansilva/placardageral/presentation/game/types/TrucoScoreActivity.kt
@@ -1,21 +1,23 @@
 package com.alansilva.placardageral.presentation.game.types
 
-import android.content.ActivityNotFoundException
-import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import com.alansilva.placardageral.R
 import com.alansilva.placardageral.databinding.ActivityTrucoScoreBinding
+import com.alansilva.placardageral.presentation.game.GameViewModel
 
 class TrucoScoreActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityTrucoScoreBinding
+    private lateinit var vewModel: GameViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setUpBinding()
         setUpListeners()
+        //initObserver()
 
     }
 
@@ -24,58 +26,102 @@ class TrucoScoreActivity : AppCompatActivity() {
         setContentView(binding.root)
     }
 
+    // SETUP OBSERVERS
+    private fun initObserver() {
+
+        vewModel.goalHome.observe(this, Observer { goalHome ->
+            binding.playerOnePlacar.text = "$goalHome"
+        })
+
+        vewModel.goalAway.observe(this, Observer {
+            binding.playerTwoPlacar.text = "$it"
+        })
+
+    }
+
     // SETUP LISTENERS
     private fun setUpListeners() {
 
-        //WhatsApp Button
-        binding.btShareWhatsApp?.setOnClickListener {
+        //Home -1
+        binding.btPOneMinus1.setOnClickListener {
+            vewModel.goalHome(-1)
+        }
+
+        //Home 1
+        binding.btPOnePlus1.setOnClickListener {
+            vewModel.goalHome()
+        }
+
+        //Home 3
+        binding.btPOnePlus3.setOnClickListener {
+            vewModel.goalHome(3)
+        }
+
+        //Home 6
+        binding.btPOnePlus6.setOnClickListener {
+            vewModel.goalHome(6)
+        }
+
+        //Home 9
+        binding.btPOnePlus9.setOnClickListener {
+            vewModel.goalHome(9)
+        }
+
+        //Home 12
+        binding.btPOnePlus12.setOnClickListener {
+            vewModel.goalHome(12)
+        }
+
+        //Home -1
+        binding.btPTwoMinus1.setOnClickListener {
+            vewModel.goalAway(-1)
+        }
+
+        //Home 1
+        binding.btPTwoPlus1.setOnClickListener {
+            vewModel.goalAway()
+        }
+
+        //Home 3
+        binding.btPTwoPlus3.setOnClickListener {
+            vewModel.goalAway(3)
+        }
+
+        //Home 6
+        binding.btPTwoPlus6.setOnClickListener {
+            vewModel.goalAway(6)
+        }
+
+        //Home 9
+        binding.btPTwoPlus9.setOnClickListener {
+            vewModel.goalAway(9)
+        }
+
+        //Home 12
+        binding.btPTwoPlus12.setOnClickListener {
+            vewModel.goalAway(12)
+        }
+
+        //WhatsApp Button ----
+        binding.btShareWhatsApp.setOnClickListener {
             shareWhatsApp()
         }
 
         //Finish Button
         binding.btFinishMatch.setOnClickListener {
-            //mainViewModel.restart()
+            vewModel.restart()
         }
-
-        //PlayerOne Button
-        //binding.btPlayerOneScore.setOnClickListener {
-           // mainViewModel.goalHome()
-        //}
-
-        //Player Two Button
-        //binding.btPlayerTwoScore.setOnClickListener {
-            //mainViewModel.goalAway()
-       // }
 
         //Revenge Button
         binding.btRevenge.setOnClickListener {
-            //mainViewModel.restart()
+            vewModel.restart()
         }
 
     }
 
     // CALL WHATSAPP APPLICATION
     private fun shareWhatsApp() {
-//        try {
-//            val whatsAppIntent = Intent(Intent.ACTION_SEND)
-//            whatsAppIntent.type = "text/plain"
-//            whatsAppIntent.setPackage("com.whatsapp")
-//            val message = getString(R.string.message_to_share,
-//                binding.tvPlayerOneName.text,
-//                binding.tvPlayerTwoName.text,
-//                binding.tvPlayerOneScore.text.toString().toInt(),
-//                binding.tvPlayerTwoScore.text.toString().toInt())
-//            whatsAppIntent.putExtra(Intent.EXTRA_TEXT, message)
-//            startActivity(whatsAppIntent)
-//        } catch (e: ActivityNotFoundException) {
-//            //Toast.makeText(this, "WhatsApp não instalado", Toast.LENGTH_LONG).show()
-//            val appPackageName = "com.whatsapp"
-//            try {
-//                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$appPackageName")))
-//            } catch (anfe: android.content.ActivityNotFoundException) {
-//                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=$appPackageName")))
-//            }
-//        }
+
     }
 
 }
